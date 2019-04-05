49efdb9892350e7342e6c714fa675e1bf888e9bb
j.haryson <jherson_k-f@hotmail.com>
2019-04-05 08:34:52 -0300

    ajustes erro ultimo commit

--- src/models/iot.js
+++ src/models/iot.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema()
+const Schema = mongoose.Schema
 
 
 
@@ -105,6 +105,6 @@ const iot = new Schema({
 //
 
 
-mongoose.module('iot', iot);
+mongoose.model('iot', iot);
 
 module.exports = iot;
